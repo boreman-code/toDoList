112325ebbe7b85fb0894d113258c72b00bfdbe1b
bore <stepanovsound@yandex.ru>
2021-01-11 13:22:36 +0700

    Add Task and TaskRepository to model package

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,50 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+    private String date;
+
+    public Task() {
+    }
+
+    public Task(Long id, String content, String date) {
+        this.id = id;
+        this.content = content;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
